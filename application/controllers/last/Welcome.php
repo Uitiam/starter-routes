1bc7401bd9c2ea028c08b4078bddd6ecb1433d0e
Uitiam <maitiumorton@gmail.com>
2017-02-02 22:47:51 -0800

    Added code to Last controller to display last quote in array
    Signed-off-by: Uitiam <maitiumorton@gmail.com>

--- application/controllers/last/Welcome.php
+++ application/controllers/last/Welcome.php
@@ -16,16 +16,11 @@ class Welcome extends Application
 	public function index()
 	{
 		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+		$this->data['pagebody'] = 'justone';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-		}
-		$this->data['authors'] = $authors;
+		//getting last quote
+		$record = $this->quotes->get(6);
+		$this->data = array_merge($this->data, $record);
 
 		$this->render();
 	}

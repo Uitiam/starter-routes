a8af9dda4a9c5b4821b83e08ed8b75dc8a589c78
Alex-Zielinski <alex.zielinski.bc@gmail.com>
2017-02-03 09:44:24 -0800

    added a Wise.php file. Fixed and closes #9
    Signed-off-by: Alex-Zielinski <alex.zielinski.bc@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,6 +56,7 @@ $route['translate_uri_dashes'] = FALSE;
 
 //custom routes
 $route['show/(:num)'] = 'first/gimme/$1';
+$route['comp\d+/\w*'] = 'Wise/bingo';
 $route['sleep'] = 'first/zzz';
 $route['dunno'] = function() {
     $source = '../res/morgan.jpg'; // an image you provide, outside of "oublic"!

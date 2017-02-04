0e965fc898be092c01343909999a2f6f7b015ee3
Uitiam <maitiumorton@gmail.com>
2017-02-03 18:59:51 -0800

    Closes #8 added Bingo controller for socrates quote and route
    Signed-off-by: Uitiam <maitiumorton@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,6 +55,7 @@ $route['translate_uri_dashes'] = FALSE;
 
 
 //custom routes
+$route['/wise/bingo'] = 'Bingo/index';
 $route['show/(:num)'] = 'first/gimme/$1';
 $route['comp\d+/\w*'] = 'Wise/bingo';
 $route['sleep'] = 'first/zzz';
